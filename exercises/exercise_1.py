2d174d2ebbd235d4f302c82663a53113a98e6ee7
Alex (NFinf) <121462320+AlexNFinf@users.noreply.github.com>
2024-05-08 14:08:35 +0000

    Skib

--- exercises/exercise_1.py
+++ exercises/exercise_1.py
@@ -1,2 +1,18 @@
 # Exercise 1
-# Your solution comes here
+number = int(input("Enter a 5 digit number"))
+# Extracting the digits
+digit1 = number // 10000
+print(f"{digit1 = }")
+digit2 = number // 1000 % 10
+print(f"{digit2 = }")
+digit3 = number // 100 % 10
+print(f"{digit3 = }")
+digit4 = number // 10 % 10
+print(f"{digit4 = }")
+digit5 = number // 1 % 10
+print(f"{digit5 = }")
+# Printing the digits
+first_int = str(digit1 + digit3 + digit5)
+second_int = str(digit2 + digit4)
+skib = (first_int + second_int)
+print(skib)
--- exercises/exercise_4.py
+++ exercises/exercise_4.py
@@ -1,2 +1,15 @@
 # Exercise 4
-# Your solution comes here
+# Read the input
+n = int(input("Enter a 4 digit number, if it's symetric I'll lyk: "))
+
+# Extract the digits
+digit1 = n // 1000
+digit2 = (n % 1000) // 100
+digit3 = (n % 100) // 10
+digit4 = n % 10
+
+# Calculate the result
+result = (digit1 == digit4) * (digit2 == digit3)
+
+# Print the result
+print(result)
